fdf2b8d22c1a9b68e96d4b9056114635fee9ab01
willardshikami <willardshikami@gmail.com>
2017-04-04 01:31:37 +0300

    Rock Paper Scissors Game Using Js, Plus other exercises

---
+++ RockPaperScissors.js
@@ -0,0 +1,45 @@
+//variable to take user input
+var userChoice = prompt("Rock, Paper, Scissors?");
+var computerChoice = Math.random();
+
+//if statement to give rock paper & scissors random values
+if(computerChoice < 0.34){
+    computerChoice = "Rock";    
+}else if(computerChoice <= 0.67){
+    computerChoice = "Paper";    
+}else{
+    computerChoice = "Scissors";
+}
+
+//Function to iterate throught the various choices
+function compare(choice1, choice2){
+    if(choice1 === choice2){
+        return "It's a Draw";
+    }
+    
+    if(choice1 === "Rock"){
+        if(choice2 === "Scissors"){
+            return "Rock Wins";
+        }else{
+            return "Paper Wins";   
+        }  
+    }
+    
+    if(choice1 === "Paper"){
+        if(choice2 === "Rock"){
+            return "Paper Wins";
+        }else{
+            return "Scissors Wins";   
+        }
+    }
+    
+    if(choice1 === "Rock"){
+        if(choice2 === "Scissors"){
+            return "Rock Wins";
+        }else{
+            return "Scissors Wins";    
+        }
+    }
+}
+
+compare(userChoice, computerChoice);
---
+++ arrays.js
@@ -0,0 +1,19 @@
+//An empty array
+var numArray = [];
+
+//index of arrays
+var i = 0;
+var values = 0;
+
+//the length of array to push to empty array
+numArray.length = 20;
+numArray.push(values);
+
+
+//While loop to iterate as it pushes
+while(i < numArray.length){
+    values + i;
+    console.log(values);
+    i++
+}
+
