f92919f44ca5b8e49be3e1d65bc15139409d469e
willardshikami <willardshikami@gmail.com>
2017-04-15 05:14:48 +0300

    updated file

--- objectsyntax.js
+++ objectsyntax.js
@@ -1,2 +1,10 @@
 var phonebookEntry = {};
 
+phonebookEntry.name = 'Stacey Nyandiko';
+phonebookEntry.number = 254788430186;
+
+phonebookEntry.phone = function(){
+    console.log('Calling ' + this.name + ' at ' + this.number + '...')
+};
+
+phonebookEntry.phone();
