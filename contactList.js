ce2e64a2993fb4d489d4c578cd0c3006c2b1a41f
willardshikami <willardshikami@gmail.com>
2017-04-17 01:24:06 +0300

    added two objects

---
+++ contactList.js
@@ -0,0 +1,12 @@
+var friends = {
+    bill : {firstName : 'Bill',
+            lastName : 'Murray',
+            number : '8347858945434',
+            address : ['ny', 'kisumu', 'kenya', '08283']},
+            
+    steve : {firstName : 'Steve',
+             lastName : 'Aoki',
+             number : '7781728298168',
+             address : ['tomboya', 'kisumu', 'kenya', '84639']}
+
+};  
