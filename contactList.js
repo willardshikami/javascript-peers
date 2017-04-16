05072f4129c8a14afa1a51cb4c0dd884d8a19bbb
willardshikami <willardshikami@gmail.com>
2017-04-17 02:08:01 +0300

    added search function that returns the details of the friend that is called

--- contactList.js
+++ contactList.js
@@ -1,18 +1,29 @@
-var friends = {
-    bill : {firstName : 'Bill',
-            lastName : 'Murray',
-            number : '8347858945434',
-            address : ['ny', 'kisumu', 'kenya', '08283']},
+var friends = {}
+    friends.bill = {
+        firstName : 'Bill',
+        lastName : 'Murray',
+        number : '8347858945434',
+        address : ['ny', 'kisumu', 'kenya', '08283']};
             
-    steve : {firstName : 'Steve',
-             lastName : 'Aoki',
-             number : '7781728298168',
-             address : ['tomboya', 'kisumu', 'kenya', '84639']}
-
-};  
+    friends.steve = { 
+        firstName : 'Steve',
+        lastName : 'Aoki',
+        number : '7781728298168',
+        address : ['tomboya', 'kisumu', 'kenya', '84639']};
 
 var list = function(obj) {
   for(var prop in obj) {
     console.log(prop);
   }
 };
+
+var search = function(name){
+    for(var prop in friends){
+    if(friends[prop].firstName === name){
+        console.log(friends[prop]);
+        return friends[prop];
+    }
+    }    
+};
+
+search ('Steve');
